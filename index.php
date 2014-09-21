3b497758b6723f113fceeaacdf059663e1fe8a91
Seth Jones <seth.jones@nerdheroes.com>
2014-09-21 16:33:01 -0400

    Changes to google code

---
+++ index.php
@@ -0,0 +1,11 @@
+<head>
+<link rel="stylesheet" type="text/css" href="/css/main.css">
+</head>
+<body>
+<div class="body-wrapper">
+This is a test of the body wrapper.
+</div>
+<div class="footer-wrapper">
+This is a test of the footer wrapper.
+</div>
+</body>
