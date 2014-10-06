dddba431dd0b9e12c3701b83664df44f4be87c45
Seth Jones <seth.jones@nerdheroes.com>
2014-10-06 18:51:28 -0400

    css changes

--- index.php
+++ index.php
@@ -1,29 +1,12 @@
-<!-- Navigation -->
-<script type="text/javascript">
-(function() {
-	var $body = document.body
-	, $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
-
-	if ( typeof $menu_trigger !== 'undefined' ) {
-		$menu_trigger.addEventListener('click', function() {
-			$body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
-		});
-	}
-
-}).call(this);
-</script>
-<nav id="slide-menu">
-	<ul>
-		<li class="timeline">Timeline</li>
-		<li class="events">Events</li>
-		<li class="calendar">Calendar</li>
-		<li class="sep settings">Settings</li>
-		<li class="logout">Logout</li>
-	</ul>
-</nav>
-<!-- Content panel -->
-<div id="content">
-	<div class="menu-trigger"></div>
-	<h1>iOS style sliding menu</h1>
-	<p>Here's an easy way to create an iOS style, side sliding menu.</p>
-</div>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>House M.G.R.</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<div class="headerbar">
+		Test Text
+		</div>
+	</body>
+</html>
