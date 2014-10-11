c98fbd65ae490b8026d54e862a92bdf971b2b340
Seth Jones <seth.jones@nerdheroes.com>
2014-10-11 16:03:05 -0400

    Revert "Css updates"
    This reverts commit 731c6ebdf533e5e560b602e211a8e609cc82faae.
    
    Conflicts:
    	css/main.css
    	index.php

--- app/init.php
+++ app/init.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-
-set_include_path(get_include_path() . PATH_SEPARATOR . 'google-api-php-client/src');
+set_include_path( get_include_path() . PATH_SEPARATOR . 'google-api/src/' );
+require_once 'google-api/src/Google/Client.php';
 require_once 'classes/GoogleAuth.php';
-?>
+
