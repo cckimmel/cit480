7163c710986f92919e395867b51868a77f4453d6
tpdickson <tpdickso@oakland.edu>
2014-10-21 13:43:54 -0400

    main content updates
    added in seth's email for agenda presentation.
    
    added in basic task list.

--- main-content.php
+++ main-content.php
@@ -7,10 +7,12 @@ include 'stickynotes/demo.php';
 
 <div class="tasklist-widget">
 <?php
-//include 'taskslist.php';
+include 'taskslist.php';
 ?>
 </div>
 
-<iframe src="https://www.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=tdicks0292%40gmail.com&amp;color=%23182C57&amp;ctz=America%2FNew_York"
+<iframe src="https://www.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=seth.jones%40nerdheroes.com&amp;color=%23182C57&amp;ctz=America%2FNew_York"
 		style=" border-width:0 " width="30%" height="70%" frameborder="0" scrolling="no" postion="absolute" float="right" right="0px">
 </iframe>
+
+<!---tdicks0292%40gmail.com&amp;color=%23182C57&amp;ctz=America%2FNew_York"--->
---
+++ taskslist.php
@@ -0,0 +1,33 @@
+<html>
+<link rel="stylesheet" type="text/css" href="css/taskslist.css">
+<body>
+<table class="tasks-list">
+<thead><tr><th colspan="2">Shopping List</th></tr></thead>
+<tbody>
+<tr>
+<td><input type="checkbox" name="chkUtil" value="itm1">2 dozen eggs for friday</td>
+</tr>
+<tr>
+<td><input type="checkbox" name="chkUtil" value="itm2">gallon of milk</td>
+</tr>
+<tr>
+<td><input type="checkbox" name="chkUtil" value="itm3">2 boxes of wheaties</td>
+</tr>
+<tr>
+<td><input type="checkbox" name="chkUtil" value="itm4">bagels + cream cheese</td>
+</tr>
+<tr>
+<td><input type="checkbox" name="chkUtil" value="itm5">bag of honeycrisp apples</td>
+</tr>
+<tr>
+<td><input type="checkbox" name="chkUtil" value="itm6">case of water bottles</td>
+</tr>
+<tr>
+<td colspan="3"><br></td>
+</tr>
+<tr>
+<td><button type="button">Clear completed</button></tr>
+</td>
+</tbody>
+</table>
+</html>
