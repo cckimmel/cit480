fda44e43666a024621818dceeefd3f5c405be2a4
cckimmel <yeaitskimmel@gmail.com>
2014-10-26 14:05:26 -0400

    Side menu design, need to match colors to site, semblance of links

--- left-menu.php
+++ left-menu.php
@@ -2,22 +2,25 @@
 ?>
 <html>
 <head>
+<!--Seths instructions to link, using logout/household as trial
+index.php?page=page.php
+Mysql connection is already present and directed at the correct database.-->
 <link rel="stylesheet" href="leftmenu.css" />
 <style type="text/css"></style>
 </head>
 <!-- Navigation -->
 <nav id="slide-menu">
    <ul class="sidemenu">
-	<li><a href="household.php" class="grey">Manage My Household</a></li>
-        <li><a href="Calendar.php" class="blue">Calendar</a></li>
+	<li><a href="index.php?household=household.php" class="grey">Manage My Household</a></li>
+    <li><a href="Calendar.php" class="blue">Calendar</a></li> <!--NotRealLink-->
 	<li><a href="Chores.html" class="blue">Chores</a></li>
-        <li><a href="Budgets.html" class="blue">Budgets</a></li>
-        <li><a href="Bills.php" class="blue">Bills</a></li>
-        <li><a href="Shopping.php" class="blue">Shopping</a></li>
-        <li><a href="FoodTracking.php" class="blue">Food Tracking</a></li>
-        <li><a href="MediaTracking.php" class="blue">Media Tracking</a></li>
-        <li><a href="Subscriptions.php" class="blue">Subscriptions</a></li>
-        <li><a href="logout.php" class="grey">Logout</a></li>
+    <li><a href="Budgets.html" class="blue">Budgets</a></li>
+    <li><a href="Bills.php" class="blue">Bills</a></li><!--NotRealLink-->
+    <li><a href="Shopping.php" class="blue">Shopping</a></li><!--NotRealLink-->
+    <li><a href="FoodTracking.php" class="blue">Food Tracking</a></li><!--NotRealLink-->
+    <li><a href="MediaTracking.php" class="blue">Media Tracking</a></li><!--NotRealLink-->
+    <li><a href="Subscriptions.php" class="blue">Subscriptions</a></li><!--NotRealLink-->
+    <li><a href="index.php?logout=logout.php" class="grey">Logout</a></li>
    </ul>
 </nav>
 </html>
