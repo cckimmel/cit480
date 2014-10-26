3fbf4898c9366c593dc80ec1aacb1623fbf1fdfe
cckimmel <yeaitskimmel@gmail.com>
2014-10-26 13:37:03 -0400

    First Menu Placeholder

--- left-menu.php
+++ left-menu.php
@@ -1,14 +1,33 @@
 <?php 
 ?>
 <html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+a {
+    display: block;
+    width: 150px;
+    background-color: #5F9EA0;
+}
+</style>
+</head>
 <!-- Navigation -->
 <nav id="slide-menu">
-	<ul>
-		<li class="timeline">Timeline</li>
-		<li class="events">Events</li>
-		<li class="calendar">Calendar</li>
-		<li class="sep settings">Settings</li>
-		<li class="logout">Logout</li>
-	</ul>
+   <ul>
+	<li><a href="household.php">Manage My Household</a></li>
+        <li><a href="Calendar.php">Calendar</a></li>
+	<li><a href="Chores.html">Chores</a></li>
+        <li><a href="Budgets.html">Budgets</a></li>
+        <li><a href="Bills.php">Bills</a></li>
+        <li><a href="Shopping.php">Shopping</a></li>
+        <li><a href="FoodTracking.php">Food Tracking</a></li>
+        <li><a href="MediaTracking.php">Media Tracking</a></li>
+        <li><a href="Subscriptions.php">Subscriptions</a></li>
+        <li><a href="logout.php">Logout</a></li>
+   </ul>
 </nav>
 </html>
